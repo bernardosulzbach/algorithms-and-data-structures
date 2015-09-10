f87981e468b4ce99f2d56b50574f05d03022b6d0
Bernardo Sulzbach <mafagafogigante@gmail.com>
2015-09-10 17:43:51 -0300

    Added random_choice to random.

--- random/random.cpp
+++ random/random.cpp
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <functional>
+#include <vector>
 
 std::mt19937 make_global_urng() {
     std::array<unsigned int, std::mt19937::state_size> seed_data;
@@ -14,4 +15,25 @@ std::mt19937 make_global_urng() {
 std::mt19937 &global_urng() {
     static std::mt19937 mt = make_global_urng();
     return mt;
+}
+
+template <typename T>
+void call_random_choice_on_vector(const std::vector<T> &v) {
+    random_choice<T>(v.begin(), v.end());
+}
+
+#include <iostream>
+int main() {
+    std::vector<int> vector;
+    try {
+        call_random_choice_on_vector(vector);
+    }
+    catch (const std::invalid_argument&) {
+        // Expected.
+    }
+    vector.push_back(1);
+    call_random_choice_on_vector(vector);
+    vector.push_back(2);
+    call_random_choice_on_vector(vector);
+    return 0;
 }
--- random/random.h
+++ random/random.h
@@ -10,3 +10,12 @@
 // Returns the global uniform random number generator.
 // Pass it to a distribution to get pseudo random numbers in the range you need.
 std::mt19937 &global_urng();
+
+template <typename T, typename It>
+T random_choice(It begin, It end) {
+    auto distance = std::distance(begin, end);
+    if (distance == 0) throw std::invalid_argument("distance between begin and end is 0.");
+    else if (distance == 1) return *begin;
+    std::uniform_int_distribution<> dist(0, distance - 1);
+    return *std::next(begin, dist(global_urng()));
+}
